13d69d0695efe518f2da686871018ef173a4856f
Sumin Park <clappingmin@gmail.com>
2022-02-11 17:51:08 +0900

    컴포넌트 설계

---
+++ rabbit-market/src/components/CommentList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentList = () => {
+  return <></>;
+};
+
+export default CommentList;
---
+++ rabbit-market/src/components/CommentWrite.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentWrite = () => {
+  return <></>;
+};
+
+export default CommentWrite;
