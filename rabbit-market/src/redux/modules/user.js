f40f9fd273fb3944c27e1e83bbedc221df22e512
Sumin Park <clappingmin@gmail.com>
2022-02-12 19:58:30 +0900

    redux 파일 설계

--- rabbit-market/src/redux/modules/user.js
+++ rabbit-market/src/redux/modules/user.js
@@ -33,114 +33,6 @@ const user_initial = {
   user_name: 'jun park',
 };
 
-//middleware actions
-const loginFB = (id, pwd) => {
-  return function (dispatch, geState, { history }) {
-    setPersistence(auth, browserSessionPersistence)
-      .then((res) => {
-        signInWithEmailAndPassword(auth, id, pwd)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            console.log(user, '여기 값이야?');
-
-            dispatch(
-              setUser({
-                user_name: user.displayName,
-                id: id,
-                user_profile: '',
-                uid: user.uid,
-              })
-            );
-
-            history.push('/');
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-
-            console.log(errorCode, errorMessage);
-          });
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-  };
-};
-
-const signupFB = (id, pwd, user_name) => {
-  return function (dispatch, getState, { history }) {
-    auth
-      .createUserWithEmailAndPassword(id, pwd)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-
-        console.log(user);
-
-        updateProfile(auth.currentUser, {
-          displayName: user_name,
-        })
-          .then(() => {
-            // Profile updated!
-            dispatch(
-              setUser({
-                user_name: user_name,
-                id: id,
-                user_profile: '',
-                uid: user.uid,
-              })
-            );
-            history.push('/');
-          })
-          .catch((error) => {
-            // An error occurred
-            console.log(error);
-          });
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        console.log(errorCode, errorMessage);
-        // ..
-      });
-  };
-};
-
-const loginCheckFB = () => {
-  return function (dispatch, getState, { history }) {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        dispatch(
-          setUser({
-            user_name: user.displayName,
-            user_profile: '',
-            id: user.email,
-            uid: user.uid,
-          })
-        );
-      } else {
-        dispatch(logOut());
-      }
-    });
-  };
-};
-
-const logoutFB = () => {
-  return function (dispatch, getstate, { history }) {
-    auth.signOut().then(() => {
-      dispatch(logOut());
-      //push  는 뒤로가기가 된다 그래서 replace로 뒤로가기해도 페이지 안나오게
-      history.replace('/');
-    });
-  };
-};
-
 //reducer
 export default handleActions(
   {
@@ -165,10 +57,6 @@ export default handleActions(
 const actionCreators = {
   logOut,
   getUser,
-  loginFB,
-  signupFB,
-  loginCheckFB,
-  logoutFB,
 };
 
 export { actionCreators };
