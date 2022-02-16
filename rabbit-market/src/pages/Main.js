c1c9392895a03d724ced11f4f16aba22fa143c7e
Sumin Park <clappingmin@gmail.com>
2022-02-16 13:51:03 +0900

    메인화면 이미지 대신 Carousels 추가

--- rabbit-market/src/pages/Main.js
+++ rabbit-market/src/pages/Main.js
@@ -4,6 +4,8 @@ import Post from '../components/Post';
 import Image from '../elements/Image';
 import Button from '../elements/Button';
 
+import ControlledCarousel from '../components/ControlledCarousel';
+
 import Text from '../elements/Text';
 
 import { useSelector, useDispatch } from 'react-redux';
@@ -27,17 +29,11 @@ const Main = (props) => {
 
   return (
     <>
-      <Grid height="10%">
-        <img
-          src="https://user-images.githubusercontent.com/82128525/153698807-45fde35c-0ce8-499b-864b-14d6b439968d.jpeg"
-          alt="나이키 이미지"
-          style={{ height: '20rem', width: '100%' }}
-        />
-      </Grid>
+      <ControlledCarousel />
+
       <Grid padding="2vw 13vw 0vw 13vw">
         <Text size="2rem">상품들이 깡충깡충</Text>
       </Grid>
-
       <Grid padding="2vw 13vw 0vw 13vw" is_grid is_wrap>
         {post_list.map((p, idx) => {
           return (
